1de22beac32d9c375c49194bb3949137bc978124
Agustin <eliasagustin@gmail.com>
2022-10-25 19:58:41 -0300

    final de seteado de angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  supabaseUrl: "https://mawrynqwarhqiidabfdz.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hd3J5bnF3YXJocWlpZGFiZmR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYyODk4ODIsImV4cCI6MTk4MTg2NTg4Mn0._ceUgNIltOcuVsPInnkSmlh4K7NtW0j4Dc3RbCglijk",
 };
 
 /*
