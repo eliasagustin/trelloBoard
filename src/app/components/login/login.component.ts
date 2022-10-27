58f3f1b90b145227e555c5720a002a9786e9db66
Agustin <eliasagustin@gmail.com>
2022-10-27 18:39:45 -0300

    working on logIn and service almost finished

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = 'isacc.shain@fallinhay.com';
+  linkSuccess = false;
+
+  constructor(private auth: AuthService,
+    private spinner: NgxSpinnerService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  async signIn() {
+    this.spinner.show();
+    const result = await this.auth.signInWithEmail(this.email);
+    console.log("~ file: login.component.ts ~ line 26 ~ LoginComponent ~ signIn ~ result", result);
+
+    this.spinner.hide();
+    if (!result.error) {
+      this.linkSuccess = true;
+    } else {
+      alert(result.error.message);
+    }
+  };
 }
