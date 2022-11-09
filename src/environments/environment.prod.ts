a925532b709d85633fbde27838ccc42a2904ff3c
Agustin <eliasagustin@gmail.com>
2022-11-09 19:06:57 -0300

    setting up things to deploy on github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: "https://mawrynqwarhqiidabfdz.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hd3J5bnF3YXJocWlpZGFiZmR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYyODk4ODIsImV4cCI6MTk4MTg2NTg4Mn0._ceUgNIltOcuVsPInnkSmlh4K7NtW0j4Dc3RbCglijk",
 };
